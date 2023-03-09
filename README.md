4191829534daac9ee05621e6a8427644f83e3512
famu8 <fmutheone@gmail.com>
2023-03-09 10:52:42 +0100

    Commit primer ejercicio

---
+++ README.md
@@ -0,0 +1,14 @@
+# C# LABORATORY
+Crea una aplicación de consola por cada uno de los siguientes enunciados:
+
+1 - Solicite el nombre de una persona, su edad y el nombre de una ciudad. Después de solicitar estos datos deberá mostrar por pantalla el siguiente mensaje: Te llamas y tienes <años> años. Bienvenido a
+
+2 – Solicite dos números y diga cual es el mayor.
+
+3 - Pedir el nombre de la semana al usuario y decirle si es fin de semana o no. En caso de error, indicarlo.
+
+4 - Recorre los números del 1 al 100. Muestra los números pares. Usar el tipo de bucle que quieras.
+
+5 – Solicitar un número al usuario y generar un Array con N números aleatorios. Por ejemplo, si el usuario introduce 4, el resultado por consola debería ser: 23, 34, 234, 11
+
+6 – Solicitar un número al usuario y un carácter. Crear una pirámide con N filas y el carácter solicitado. Por ejemplo, si el usuario introduce 5 y * el resultado por consola debería ser:
---
+++ ej1/ejercicio1-solution/ejercicio1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+/*
+ 1 - Solicite el nombre de una persona, su edad y el nombre de una ciudad. 
+Después de solicitar estos datos deberámostrar por pantalla el siguiente mensaje: 
+Te llamas y tienes <años> años. Bienvenido a
+*/
+
+namespace ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solicitar el nombre
+            Console.WriteLine("¿Cual es tu nombre?");
+            string name = Console.ReadLine();
+            //Solicitar la edad
+            Console.WriteLine("¿Cual es tu edad?");
+            string age = Console.ReadLine();
+            //Convertir la edad
+            age.Cast<int>();
+            //Solicitar la ciudad
+            Console.WriteLine("Introduce una ciudad: ");
+            var city = Console.ReadLine();
+            //Mostrar el mensaje
+            Console.WriteLine($"Te llamas {name} y tienes {age} años. Bienvenido a {city}");
+        }
+    }
+}
+
+
