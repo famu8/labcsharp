8c9ce00c55ed046bef497bd1b2dc810bc9bbb4b4
famu8 <fmutheone@gmail.com>
2023-03-09 11:23:41 +0100

    Ejercicios 2 y 3 hechos

---
+++ ej2/ejercicio2-solution/ejercicio2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace ejercicio2
+{
+    internal class Program{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un primer numero: ");
+            string num1 = Console.ReadLine();
+            int numberOne = Int32.Parse(num1);
+            Console.WriteLine("Introduce un segundo numero: ");
+            string num2 = Console.ReadLine();
+            int numberTwo = Int32.Parse(num2);
+
+            Console.WriteLine($"Tu primero numero es: {numberOne}");
+            Console.WriteLine($"Tu segundo numero es: {numberTwo}");
+
+            if (numberOne > numberTwo)
+            {
+                Console.WriteLine("------------------");
+                Console.WriteLine($"| {numberOne} es mayor que {numberTwo}|");
+                Console.WriteLine("------------------");
+
+                Console.WriteLine("Adios!");
+            }
+            else
+            {
+                Console.WriteLine("------------------");
+                Console.WriteLine($"| {numberTwo} es mayor que {numberOne}|");
+                Console.WriteLine("------------------");
+
+                Console.WriteLine("Adios!");
+            }
+
+
+        }
+
+    }
+}
---
+++ ej3/ejercicio3-solution/ejercicio3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+/*
+3 - Pedir el nombre de la semana al usuario y decirle si es fin de semana o no. En caso de error, indicarlo.
+*/
+
+namespace ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solicitar el nombre
+            Console.WriteLine("Introduce un dia de la semana");
+            string name = Console.ReadLine();
+            switch (name)
+            {
+
+                case "Lunes":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+
+                case "Martes":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "Miercoles":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "Jueves":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "Viernes":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "Sabado":
+                    Console.WriteLine("Es fin de semana");
+                    break;
+                case "Domingo":
+                    Console.WriteLine("Es fin de semana");
+                    break;
+                default:
+                    Console.WriteLine("Error...");
+                    break;
+            }
+
+        }
+    }
+}
+
+
