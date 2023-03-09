7f1afa5a56c12d501644a5723e8cf63f592dbef0
famu8 <fmutheone@gmail.com>
2023-03-09 18:31:00 +0100

    Commit 4 ejercicio

---
+++ ej4/ejercicio4-solution/ejercicio4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*
+4 - Recorre los números del 1 al 100. Muestra los números pares. Usar el tipo de bucle que quieras.
+ */
+
+namespace ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if(i%2==0)
+                {
+                    Console.WriteLine($"Numero: {i}");
+                }
+            }
+        }
+
+    }
+}
